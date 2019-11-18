6d68dc8e8bc6c9ba249c589a20561d18b35f7909
raje reddy <raje1reddy@gmail.com>
2019-11-18 16:47:59 +0000

    code for multiple  interface default method  example

--- src/test/java/com/padmaja/flowcontrol/AnimalTest.java
+++ src/test/java/com/padmaja/flowcontrol/AnimalTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AnimalTest {
-
+Animal animal = new Animal();
     @Test
     public void relax() {
         System.out.println("relaxation");
-
+        animal.relax();
+        animal.relax();
     }
+
 }
